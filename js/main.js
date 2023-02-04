7729fb7e19a0d47921b7258a16fcb557660d5723
gr413_grkva <grebenuk13@yandex.ru>
2023-02-04 10:28:19 +0700

    первое создание заметок

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
 let eventBus = new Vue()
+let app = new Vue({
+   el:'#app',
+        data: {
+                name: null,
+                review: null,
+                rating: null,
+            reviews: [],
+            },
+        methods: {
+            onSubmit() {
+                if (this.name && this.review && this.rating) {
+                    let productReview = {
+                        name: this.name,
+                        review: this.review,
+                        rating: this.rating,
+                    }
+                    eventBus.$emit('review-submitted', productReview)
+                    this.name = null
+                    this.review = null
+                    this.rating = null
+                }
+            },
+        },
+    mounted() {
+        eventBus.$on('review-submitted', productReview => {
+            this.reviews.push(productReview)
+        })
+    },
+})
