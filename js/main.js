71213ad65c464f78409750ba8bf6e7392939529f
gr413_grkva <grebenuk13@yandex.ru>
2023-02-08 11:04:26 +0700

    пытаюсь чёт сделать

--- js/main.js
+++ js/main.js
@@ -7,18 +7,38 @@ let app = new Vue({
                 name: null,
                 review: null,
                 rating: null,
+
+            name2:null,
+            review2:null,
+            rating2:null,
+
             reviews: [],
+            reviews2: [],
             },
         methods: {
        hideButton() {
          this.visibility = false
        },
+            onSubmit2() {
+           if (this.name2 && this.review2 && this.rating2) {
+               let productReview2 = {
+                   name2: this.name2,
+                   review2: this.review2,
+                   rating2: this.rating2,
+               }
+               eventBus.$emit('review-submitted', productReview)
+               this.name2 = null
+               this.review2 = null
+               this.rating2 = null
+           }
+            },
             onSubmit() {
                 if (this.name && this.review && this.rating) {
                     let productReview = {
                         name: this.name,
                         review: this.review,
                         rating: this.rating,
+
                     }
                     eventBus.$emit('review-submitted', productReview)
                     this.name = null
@@ -26,10 +46,24 @@ let app = new Vue({
                     this.rating = null
                 }
             },
+
         },
+    // mounted2() {
+    //    eventBus.$on('review-submitted2', productReview2 => {
+    //   this.reviews2.push(productReview2)
+    //   console.log(this.reviews2.length)
+    //    })
+    // },
     mounted() {
         eventBus.$on('review-submitted', productReview => {
             this.reviews.push(productReview)
+            console.log(this.reviews.length)
+            if (this.reviews.length > 1) {
+                this.reviews.pop(productReview)
+
+                    this.reviews2.push(productReview)
+                    console.log(this.reviews2.length)
+            }
         })
     },
 })
